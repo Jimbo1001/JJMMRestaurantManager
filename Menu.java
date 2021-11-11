9b903be93c9bbdeb87a817a780f2e8ebbf03f8d7
Marcelo110 <91700924+Marcelo110@users.noreply.github.com>
2021-11-11 13:08:57 -0400

    Modification
    I slightly modified the code to make it look nicer and added a toString() method (which is currently unfinished).

--- Menu.java
+++ Menu.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
 
-class Menu{
+class Menu {
     String name;
     ArrayList<MenuItem> menuItems = new ArrayList<MenuItem>();
-    Menu( String name ){
+    Menu(String name) {
         this.name = name;
+    }
+    public String toString() {
+        String s = "";
+        return s;
     }
 }

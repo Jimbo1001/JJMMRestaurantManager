2338421118d7233748d4b050bba498860746441c
Marcelo110 <91700924+Marcelo110@users.noreply.github.com>
2021-11-11 16:00:17 -0400

    Update MenuItem.java

--- MenuItem.java
+++ MenuItem.java
@@ -1,18 +1,26 @@
 import java.util.ArrayList;
 
-class MenuItem{
+class MenuItem {
     String name;
     float price;
     ArrayList<MenuCustomizationItem> menuCustomizationItems = new ArrayList<MenuCustomizationItem>();
     
-    MenuItem( String name, float price ){
+    MenuItem(String name, float price) {
         this.name = name;
         this.price = price;
     }
 
-    public void setPrice( float price ){
-        if ( price >= 0 ){
+    public void setPrice(float price) {
+        if (price >= 0) {
             this.price = price;
         }
+    }
+    
+    public String toString() {
+        String results = "+";
+        for (int i = 0; i < this.menuCustomizationItem.size; i++) {
+            results += " " + this.menuCustomizationItem.get(i);
+        }
+        return results;
     }
 }

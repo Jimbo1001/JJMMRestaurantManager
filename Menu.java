96f28fa504939155040a48db42d2ca50e957ffa3
Marcelo110 <91700924+Marcelo110@users.noreply.github.com>
2021-11-11 16:05:17 -0400

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -3,11 +3,16 @@ import java.util.ArrayList;
 class Menu {
     String name;
     ArrayList<MenuItem> menuItems = new ArrayList<MenuItem>();
+    
     Menu(String name) {
         this.name = name;
     }
+    
     public String toString() {
-        String s = "";
-        return s;
+        String results = "+";
+        for (int i = 0; i < this.menuItem.size; i++) {
+            results += " " + this.menuItem.get(i);
+        }
+        return results;
     }
 }

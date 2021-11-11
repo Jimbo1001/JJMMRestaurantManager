14f4a8f6bc559ccf64d7102db862e39f193b8f5e
Marcelo110 <91700924+Marcelo110@users.noreply.github.com>
2021-11-11 15:58:40 -0400

    Update MenuCustomizationItem.java

--- MenuCustomizationItem.java
+++ MenuCustomizationItem.java
@@ -1,14 +1,20 @@
-class MenuCustomizationItem{
+class MenuCustomizationItem {
     String name;
     float price;
-    MenuCustomizationItem( String name, float price ){
+    
+    MenuCustomizationItem(String name, float price) {
         this.name = name;
         this.price = price;
     }
 
-    public void setPrice( float price ){
-        if ( price >= 0 ){
+    public void setPrice(float price) {
+        if (price >= 0) {
             this.price = price;
         }
+    }
+    
+    public String toString() {
+        String s = "ITEM NAME: " + name + "\nPRICE: " + price;
+        return s;
     }
 }

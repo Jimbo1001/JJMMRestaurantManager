81a9889f3dc5b957e276cee9c951f696d0f43585
MugdhaYada <93006212+MugdhaYada@users.noreply.github.com>
2021-11-28 21:04:21 -0500

    Added methods to sort the items in according to name and price

--- MenuItem.java
+++ MenuItem.java
@@ -14,7 +14,39 @@ class MenuItem {
             this.price = price;
         }
     }
+// Returns a sorted array list of menuItems either in ascending or descending
+	// order of the names
+	public static ArrayList<MenuCustomizationItem> sortByName(ArrayList<MenuCustomizationItem> menuItems,
+			String sortBy) {
+		// using lambda expression to sort in ascending order
+		if (sortBy.toLowerCase() == "descending") {
+			Collections.sort(menuItems, (p1, p2) -> {
+				return p2.name.compareTo(p1.name);
+			});
+		}
+		// sorting in ascending order by default
+		else {
+			Collections.sort(menuItems, (p1, p2) -> {
+				return p1.name.compareTo(p2.name);
+			});
+		}
+		return menuItems;
+	}
 
+	// Returns a sorted array list of menuItems either in ascending or descending
+	// order of the prices
+	public static ArrayList<MenuCustomizationItem> sortByPrice(ArrayList<MenuCustomizationItem> menuItems,
+			String sortBy) {
+		// using lambda expression to sort in ascending order
+		if (sortBy.toLowerCase() == "descending") {
+			Collections.sort(menuItems, (p1, p2) -> Float.compare(p2.price, p1.price));
+		}
+		// sorting in ascending order by default
+		else {
+			Collections.sort(menuItems, (p1, p2) -> Float.compare(p1.price, p2.price));
+		}
+		return menuItems;
+	}
     /*
     @Override
     public boolean equals( MenuItem o ){

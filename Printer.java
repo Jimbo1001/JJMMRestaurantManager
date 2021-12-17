b5bebce207cb763b791d8b1df0fc66b692260cb5
MugdhaYada <93006212+MugdhaYada@users.noreply.github.com>
2021-12-17 13:10:35 -0500

    Add the method to print the text file

--- Printer.java
+++ Printer.java
@@ -1,4 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 class Printer{
+   public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		FileReader fr = new FileReader("testfile.txt");
+		BufferedReader br = new BufferedReader(fr);
+		String buffer;
+        String fulltext="";
+        while ((buffer = br.readLine()) != null) {
+            System.out.println(buffer);
+            fulltext += buffer;
+        }
+        br.close();
+        fr.close();
+	}
    
 }

3d80eee2d9f512b23b9784db8f60ec584477c07c
Marcelo110 <91700924+Marcelo110@users.noreply.github.com>
2021-12-18 15:17:49 -0400

    Update PrinterPage.java
    Made the code look a little nicer.

--- PrinterPage.java
+++ PrinterPage.java
@@ -4,13 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-class PrinterPage{
+class PrinterPage {
     JPanel panel = new JPanel();
     GridBagConstraints c = new GridBagConstraints();
 
-    JPanel buttonPanel  = new JPanel();//panel with buttons
-    JPanel splitPanel   = new JPanel();//panel with bill splitting options
-    JPanel receiptPanel = new JPanel();//panel with receipt
+    JPanel buttonPanel  = new JPanel(); //panel with buttons
+    JPanel splitPanel   = new JPanel(); //panel with bill splitting options
+    JPanel receiptPanel = new JPanel(); //panel with receipt
     
     Dimension buttonSize;
     Dimension panelSize;
@@ -21,107 +21,107 @@ class PrinterPage{
     private int splitAmt = 1;
 
     Dimension labelSize;
-    PrinterPage( int w, int h ){
+    PrinterPage(int w, int h) {
         labelSize = new Dimension( (int)(w/5) / 2, (int)(h/15) / 2 );
-        panel.setLayout( new GridBagLayout() );
-        panel.setPreferredSize( new Dimension(w, h) );
+        panel.setLayout(new GridBagLayout());
+        panel.setPreferredSize(new Dimension(w, h));
         
-        buttonPanel.setLayout( new GridBagLayout() );
-        buttonPanel.setPreferredSize( new Dimension( new Dimension( (int)(w - (w/50)), (int)(h/15) ) ) );
+        buttonPanel.setLayout(new GridBagLayout());
+        buttonPanel.setPreferredSize(new Dimension( new Dimension((int)(w - (w/50)), (int)(h/15))));
         JButton splitOneBtn = new JButton("Split Bill");
-        splitReceiptBtn.setPreferredSize( labelSize );
-        splitReceiptBtn.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ){
-                if (splitAmt<=4){
+        splitReceiptBtn.setPreferredSize(labelSize);
+        splitReceiptBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (splitAmt<=4) {
                     splitAmt++;
                 }
             }
         });
         c.gridx = 0;
         c.gridy = 0;
-        buttonPanel.add( splitReceiptBtn, c );
+        buttonPanel.add(splitReceiptBtn, c);
 
         JButton printReceiptBtn = new JButton("Print Bill");
-        printReceiptBtn.setPreferredSize( labelSize );
-        printReceiptBtn.addActionListener( new ActionListener() {
-            public void actionPerformed( ActionEvent e ){
+        printReceiptBtn.setPreferredSize(labelSize);
+        printReceiptBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 System.out.println("print receipt");
             }
         });
         c.gridx = 1;
         c.gridy = 0;
-        buttonPanel.add( printReceiptBtn, c );
+        buttonPanel.add(printReceiptBtn, c);
 
         c.gridx = 0;
         c.gridy = 0;
-        panel.add( buttonPanel, c );
+        panel.add(buttonPanel, c);
 
-        drawReceipt( w, h );
+        drawReceipt(w, h);
     }
 
-    public void drawReceipt( int w, int h ){
+    public void drawReceipt(int w, int h) {
         receiptPanel.removeAll();
         receiptPanel.repaint();
         receiptPanel.revalidate();
-        receiptPanel.setLayout( new GridBagLayout() );
+        receiptPanel.setLayout(new GridBagLayout());
         Table activeTable = RestaurantManager.restaurant.tables.get(tableIndex);
         int i = 0;
-        for ( ; i < activeTable.receipt.itemList.size(); i++){//RestaurantManager.restaurant.tables.get(tableIndex).receipt.itemList.size(); i++){
-            JLabel itemName = new JLabel( RestaurantManager.restaurant.tables.get(tableIndex).receipt.itemList.get(i).name + "" );
+        for (; i < activeTable.receipt.itemList.size(); i++) { //RestaurantManager.restaurant.tables.get(tableIndex).receipt.itemList.size(); i++){
+            JLabel itemName = new JLabel(RestaurantManager.restaurant.tables.get(tableIndex).receipt.itemList.get(i).name + "");
             c.gridx = 0;
             c.gridy = i;
-            receiptPanel.add( itemName, c );
+            receiptPanel.add(itemName, c);
 
-            JLabel itemPrice = new JLabel( (RestaurantManager.restaurant.tables.get(tableIndex).receipt.itemList.get(i).price / splitAmt)  + "" );
+            JLabel itemPrice = new JLabel((RestaurantManager.restaurant.tables.get(tableIndex).receipt.itemList.get(i).price / splitAmt) + "");
             c.gridx = 1;
             c.gridy = i;
-            receiptPanel.add( itemPrice, c );
+            receiptPanel.add(itemPrice, c);
         }
 
-        JLabel subTotalLabel1 = new JLabel( "Sub Total: " );
+        JLabel subTotalLabel1 = new JLabel("Subtotal: ");
         subTotalLabel1.setHorizontalAlignment( SwingConstants.LEFT);
-        subTotalLabel1.setPreferredSize( labelSize );
+        subTotalLabel1.setPreferredSize(labelSize);
         c.gridx = 0;
         c.gridy = i + 1;
-        receiptPanel.add( subTotalLabel1, c );
+        receiptPanel.add(subTotalLabel1, c);
 
-        JLabel subTotalLabel2 = new JLabel( "" + RestaurantManager.floatToString( RestaurantManager.restaurant.tables.get(tableIndex).receipt.getSubTotal(), true ) );
-        subTotalLabel2.setHorizontalAlignment( SwingConstants.RIGHT);
-        subTotalLabel2.setPreferredSize( labelSize );
+        JLabel subTotalLabel2 = new JLabel("" + RestaurantManager.floatToString(RestaurantManager.restaurant.tables.get(tableIndex).receipt.getSubTotal(), true));
+        subTotalLabel2.setHorizontalAlignment(SwingConstants.RIGHT);
+        subTotalLabel2.setPreferredSize(labelSize);
         c.gridx = 1;
         c.gridy = i + 1;
-        receiptPanel.add( subTotalLabel2, c );
+        receiptPanel.add(subTotalLabel2, c);
 
-        JLabel taxLabel1 = new JLabel( "Tax: " );
-        taxLabel1.setHorizontalAlignment( SwingConstants.LEFT);
-        taxLabel1.setPreferredSize( labelSize );
+        JLabel taxLabel1 = new JLabel("Tax: ");
+        taxLabel1.setHorizontalAlignment(SwingConstants.LEFT);
+        taxLabel1.setPreferredSize(labelSize);
         c.gridx = 0;
         c.gridy = i + 2;
-        receiptPanel.add( taxLabel1, c );
+        receiptPanel.add(taxLabel1, c);
 
-        JLabel taxLabel2 = new JLabel( "" + RestaurantManager.floatToString( activeTable.receipt.getTax(), true ) );
-        taxLabel2.setHorizontalAlignment( SwingConstants.RIGHT);
-        taxLabel2.setPreferredSize( labelSize );
+        JLabel taxLabel2 = new JLabel("" + RestaurantManager.floatToString(activeTable.receipt.getTax(), true));
+        taxLabel2.setHorizontalAlignment(SwingConstants.RIGHT);
+        taxLabel2.setPreferredSize(labelSize);
         c.gridx = 1;
         c.gridy = i + 2;
-        receiptPanel.add( taxLabel2, c );
+        receiptPanel.add(taxLabel2, c);
 
-        JLabel totalLabel1 = new JLabel( "Total: " );
-        totalLabel1.setHorizontalAlignment( SwingConstants.LEFT);
-        totalLabel1.setPreferredSize( labelSize );
+        JLabel totalLabel1 = new JLabel("Total: ");
+        totalLabel1.setHorizontalAlignment(SwingConstants.LEFT);
+        totalLabel1.setPreferredSize(labelSize);
         c.gridx = 0;
         c.gridy = i + 3;
-        receiptPanel.add( totalLabel1, c );
+        receiptPanel.add(totalLabel1, c);
 
-        JLabel totalLabel2 = new JLabel( "" + RestaurantManager.floatToString( activeTable.receipt.getTotal(), true ) );
-        totalLabel2.setHorizontalAlignment( SwingConstants.RIGHT);
-        totalLabel2.setPreferredSize( labelSize );
+        JLabel totalLabel2 = new JLabel("" + RestaurantManager.floatToString(activeTable.receipt.getTotal(), true));
+        totalLabel2.setHorizontalAlignment(SwingConstants.RIGHT);
+        totalLabel2.setPreferredSize(labelSize);
         c.gridx = 1;
         c.gridy = i + 3;
-        receiptPanel.add( totalLabel2, c );
+        receiptPanel.add(totalLabel2, c);
 
         c.gridx = 0;
         c.gridy = 1;
-        panel.add( receiptPanel, c );
+        panel.add(receiptPanel, c);
     }
 }
